1da5a1287f411af0089285c1018540b0d1ee43e6
Mahima <146190186+Mahimajy@users.noreply.github.com>
2025-03-11 19:27:57 +0530

    Create index.js

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,12 @@
+import PredictionComponent from "../components/PredictionComponent";
+
+function Home() {
+    return (
+        <div>
+            <h1>Smart Fishing Assistant</h1>
+            <PredictionComponent />
+        </div>
+    );
+}
+
+export default Home;
